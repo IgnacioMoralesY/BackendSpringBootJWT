2133b0630ec584573592181e499d01bfa6971d00
ignacio <ignacio.emy93@gmail.com>
2019-12-04 16:26:27 -0300

    Comit con el proyecto Backend

---
+++ spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -0,0 +1,12 @@
+package com.bolsadeideas.springboot.backend.apirest.models.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteDao extends JpaRepository<Cliente, Long>{
+
+}
